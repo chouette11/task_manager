295034a28184781fa90df6e0783dc8459862ff73
kei1115 <keifukuda1115@gmail.com>
2021-12-25 10:49:31 +0900

    Fix calendar system

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,9 +1,10 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:googleapis/calendar/v3.dart';
 import 'package:http/http.dart';
 import 'package:http/io_client.dart';
+import 'package:task_manager/firestore.dart';
+import 'package:task_manager/task.dart';
 
 Future<List<Widget>?> onEventButton(GoogleSignIn googleSignIn, int type) async {
   try {
@@ -16,49 +17,30 @@ Future<List<Widget>?> onEventButton(GoogleSignIn googleSignIn, int type) async {
     var calendar = CalendarApi(client);
 
     String calendarId = "primary";
-    Event event = Event();
-
-    switch (type) {
-      case 0:
-        event.summary = "開始";
-        break;
-      case 1:
-        event.summary = "終了";
-        break;
-      default:
-        break;
-    }
-
-    EventDateTime start = EventDateTime();
-    start.dateTime = DateTime.now();
-    start.timeZone = "GMT+09:00";
-    event.start = start;
-
-    EventDateTime end = EventDateTime();
-    end.timeZone = "GMT+09:00";
-    end.dateTime = DateTime.now().add(Duration(days: 1));
-    event.end = end;
 
     final events = await calendar.events.list(calendarId);
-    print(events.items![4].summary);
-    print("取得中です");
-    List<Widget> tasks = [];
+    Task? task = await getTaskFromFirestore('フクダ');
+
+    List<Map<String, dynamic>> tasksData = task!.tasks;
     events.items!.forEach((element) {
-      if (element.summary != null) {
-        tasks.add(Text(element.summary!));
+      if (element.created!.isAfter(task.pastTime)) {
+        if (element.summary != null) {
+          tasksData.add(<String, dynamic>{
+            'task': element.summary!,
+            'isFinish': false,
+          });
+        }
       }
     });
+    setTasks(tasksData);
 
-    String id = events.items![0].id!;
-
-    final book = await calendar.events.get(calendarId, id).then((value) {
-      if (value.status == "confirmed") {
-        print('イベントの追加に成功');
-      } else {
-        print("イベントの追加失敗");
+    List<Widget> tasks = [];
+    tasksData.forEach((element) {
+      print(element);
+      if (element['isFinish'] == false) {
+        tasks.add(Text(element['task']));
       }
     });
-    print(book);
     return tasks;
   } catch (e) {
     print('エラー $e');

2200674caafb33b4e4499e5e1971ed33bc1d86b8
kei1115 <keifukuda1115@gmail.com>
2021-12-30 00:05:37 +0900

    Add limit

--- lib/task.dart
+++ lib/task.dart
@@ -3,16 +3,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Task {
   Task(DocumentSnapshot doc) {
     taskId = doc.id;
+    print(doc.data());
     final data = doc.data() as Map<String, dynamic>;
-    final _tasks = data['tasks'].cast<Map<String, dynamic>>()
+    final _tasks = data['taskData'].cast<Map<String, dynamic>>()
     as List<Map<String, dynamic>>;
-    tasks = _tasks.map((item) => <String, dynamic>{
+    taskData = _tasks.map((item) => <String, dynamic>{
+      'id': item['id'] as int,
       'task': item['task'] as String,
-      'isFinish': item['isFinish'] as bool,
+      'limit' : item['limit'].toDate() as DateTime,
     }).toList();
     pastTime = data['pastTime'].toDate();
   }
   var taskId;
-  var tasks;
+  var taskData;
   var pastTime;
 }

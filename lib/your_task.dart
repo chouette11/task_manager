ed27a049dd11b3369ca353ba4c4e6633f3765c3b
kei1115 <keifukuda1115@gmail.com>
2022-01-07 17:21:46 +0900

    Add TaskPage

---
+++ lib/your_task.dart
@@ -0,0 +1,107 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:task_manager/main.dart';
+import 'package:task_manager/task_card.dart';
+
+class TaskPage extends StatefulWidget {
+  const TaskPage({Key? key}) : super(key: key);
+
+  @override
+  _TaskPageState createState() => _TaskPageState();
+}
+
+class _TaskPageState extends State<TaskPage> {
+  int pageIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TodayTask(pageIndex + 1),
+        BottomNavigationBar(
+          onTap: (index) {
+            pageIndex = index;
+            setState(() {
+
+            });
+          },
+          currentIndex: pageIndex,
+          items: [
+            BottomNavigationBarItem(
+              label: "1",
+                icon: Icon(Icons.ac_unit),),
+            BottomNavigationBarItem(
+              label: "2",
+                icon: Icon(Icons.ac_unit),),
+            BottomNavigationBarItem(
+              label: "3",
+                icon: Icon(Icons.ac_unit)),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+
+class TodayTask extends ConsumerStatefulWidget {
+  const TodayTask(this.index, {Key? key}) : super(key: key);
+  final int index;
+
+  @override
+  OneDayTaskState createState() => OneDayTaskState();
+}
+
+class OneDayTaskState extends ConsumerState<TodayTask> {
+  @override
+  Widget build(BuildContext context) {
+    final tasks = ref.watch(itemsStreamProvider);
+    var now = DateTime.now();
+
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height - 136,
+      decoration: BoxDecoration(
+          border: Border.all()
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "あなたの${widget.index}日間のタスクは",
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            tasks.when(
+              data: (items) {
+                List<Map<String, dynamic>> todayTasks = [];
+                items.taskData.forEach((element) {
+                  Duration diff = element['limit'].difference(now);
+                  int days = diff.inDays;
+                  int hours = diff.inHours - (days * 24);
+                  int minutes = diff.inMinutes - (days * 24 * 60) - (hours * 60);
+                  print(days);
+                  if (days >= 0 && days < widget.index && hours >= 0 && minutes >= 0) {
+                    todayTasks.add(element);
+                  }
+                });
+                if (todayTasks.length == 0) {
+                  return Text("ありません");
+                }
+                int random = Random().nextInt(todayTasks.length);
+                return NowTask(taskData: todayTasks[random]);
+              },
+              error: (error, _) => Text(error.toString()),
+              loading: () => const CircularProgressIndicator(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

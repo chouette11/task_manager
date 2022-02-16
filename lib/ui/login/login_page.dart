9cacee73f48bab41ef16b6093b28910e44a4caf4
kei1115 <keifukuda1115@gmail.com>
2022-02-16 17:21:45 +0900

    Add login

---
+++ lib/ui/login/login_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:task_manager/ui/components/layout/default.dart';
+import 'package:task_manager/ui/login/login_state.dart';
+import 'package:task_manager/ui/login/login_view_model.dart';
+import 'package:task_manager/ui/top/top_page.dart';
+
+class LoginPage extends ConsumerWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final viewModel = ref.watch(loginViewModelProvider.notifier);
+    final state = ref.watch(loginViewModelProvider);
+    return DefaultLayout<LoginState>(
+      state: state,
+      backgroundColor: Colors.black,
+      builder: (data) => Column(
+        children: [
+          ElevatedButton(
+            onPressed: () async {
+              await viewModel.login();
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return TopPage();
+                },
+              ));
+            },
+            child: Text("google"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              viewModel.login();
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return TopPage();
+                },
+              ),);
+            },
+            child: Text("ゲスト"),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/ui/login/login_view_model.dart
@@ -0,0 +1,56 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:task_manager/data/repository/auth/auth_repository.impl.dart';
+import 'package:task_manager/data/repository/firestore/firestore_repository.impl.dart';
+import 'package:task_manager/ui/hook/user_router.dart';
+import 'package:task_manager/ui/login/login_state.dart';
+import 'package:task_manager/ui/route/app_route.gr.dart';
+
+final loginViewModelProvider =
+StateNotifierProvider.autoDispose<LoginViewModel, AsyncValue<LoginState>>(
+      (ref) => LoginViewModel(ref: ref),
+);
+
+class LoginViewModel extends StateNotifier<AsyncValue<LoginState>> {
+  final AutoDisposeStateNotifierProviderRef _ref;
+  LoginViewModel({required AutoDisposeStateNotifierProviderRef ref})
+      : _ref = ref,
+        super(const AsyncLoading()) {
+    load();
+  }
+
+  late final authRepository = _ref.read(authRepositoryProvider);
+  late final firestoreRepository = _ref.watch(firestoreRepositoryProvider);
+
+
+  /// 通信、初期化処理
+  Future<void> load() async {
+    // Mockデータ
+    state = const AsyncValue.data(
+      LoginState(
+        email: 'eat2pic@23-creative.co.jp',
+        password: 'password',
+      ),
+    );
+  }
+
+  Future<void> login() async {
+    await authRepository.googleSignIn();
+  }
+
+  /// ロード制御
+  Future<dynamic> whileLoading(Future Function() future) {
+    return Future.microtask(toLoading)
+        .then<dynamic>((_) => future())
+        .whenComplete(toIdle);
+  }
+
+  /// ロード開始
+  void toLoading() {
+    state = AsyncValue.data(state.value!.copyWith(isLoading: true));
+  }
+
+  /// ロード終了
+  void toIdle() {
+    state = AsyncValue.data(state.value!.copyWith(isLoading: false));
+  }
+}

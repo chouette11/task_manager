acef2c48da66a86d5e0bb95e0c3c9adb206ef2a5
kei1115 <keifukuda1115@gmail.com>
2022-03-03 01:18:32 +0900

    Add showDialog

--- lib/ui/add_task/add_task_page.dart
+++ lib/ui/add_task/add_task_page.dart
@@ -1,25 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:task_manager/ui/add_task/add_task_state.dart';
+import 'package:go_router/go_router.dart';
 import 'package:task_manager/ui/add_task/add_task_view_model.dart';
+import 'package:task_manager/ui/components/form/text_field.dart';
 import 'package:task_manager/ui/components/layout/default.dart';
+import 'package:task_manager/ui/top/top_page.dart';
 
 class AddTaskPage extends ConsumerWidget {
   const AddTaskPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final state = ref.watch(AddTaskViewModelProvider);
-    final viewModel = ref.watch(AddTaskViewModelProvider.notifier);
+    final state = ref.watch(addTaskViewModelProvider);
+    final viewModel = ref.watch(addTaskViewModelProvider.notifier);
 
-    return DefaultLayout<AddTaskState>(
-      state: state,
-      backgroundColor: Colors.black,
-      builder: (data) => Column(
-        children: [
+    return state.when(
+      data: (data) {
+        return Column(
+          children: [
+            Text("タスクを入力してね"),
 
-        ],
-      ),
+            Row(
+              children: [
+                CustomTextFormField(
+                  hintText: "年",
+                  initialValue: data.task,
+                  onChanged: viewModel.onChangedTask,
+                  keyboardType: TextInputType.emailAddress,
+                ),
+
+              ],
+            ),
+
+            CustomTextFormField(
+              hintText: "タスク",
+              initialValue: data.task,
+              onChanged: viewModel.onChangedTask,
+              keyboardType: TextInputType.emailAddress,
+            ),
+
+            ElevatedButton(
+              onPressed: () => context.pop(),
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+      error: (e, stackTrace) => TopPage(),
+      loading: () => const TopPage(),
     );
   }
 }

175f527dce48f659904cc1f3c8ea10c1499fea9c
kei1115 <keifukuda1115@gmail.com>
2022-03-03 01:17:51 +0900

    Add text_field.dart

---
+++ lib/ui/components/form/text_field.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// テキスト入力欄
+class CustomTextFormField extends ConsumerWidget {
+  const CustomTextFormField({
+    Key? key,
+    this.initialValue,
+    this.validator,
+    this.onChanged,
+    this.hintText,
+    this.labelText,
+    this.obscureText = false,
+    this.keyboardType,
+  }) : super(key: key);
+  final String? initialValue;
+  final String? Function(String?)? validator;
+  final void Function(String)? onChanged;
+  final String? hintText;
+  final String? labelText;
+  final bool obscureText;
+  final TextInputType? keyboardType;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    return TextFormField(
+      initialValue: initialValue,
+      validator: validator,
+      onChanged: onChanged,
+      keyboardType: keyboardType,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      decoration: InputDecoration(
+        filled: true,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(4.0),
+        ),
+        hintText: hintText,
+        labelText: labelText,
+      ),
+    );
+  }
+}

bef99424be381f215053d385c9fa4d87ab4fe411
kei1115 <keifukuda1115@gmail.com>
2022-03-03 22:43:13 +0900

    add: onChangeTask and onChangeDate

--- lib/ui/add_task/add_task_view_model.dart
+++ lib/ui/add_task/add_task_view_model.dart
@@ -16,14 +16,45 @@ class AddTaskViewModel extends StateNotifier<AsyncValue<AddTaskState>> {
 
   /// 通信、初期化処理
   Future<void> load() async {
+    final now = DateTime.now();
     state = AsyncValue.data(
       AddTaskState(
         task: "task",
+        year: now.year,
+        month: now.month - 1,
+        day: now.day,
+        hour: now.hour,
+        minute: now.minute,
       ),
     );
   }
 
-  /// メールアドレス onChanged
+  /// 年
+  void onChangedYear(String value) {
+    state = AsyncValue.data(state.value!.copyWith(year: int.parse(value)));
+  }
+
+  /// 月
+  void onChangedMonth(String value) {
+    state = AsyncValue.data(state.value!.copyWith(month: int.parse(value)));
+  }
+
+  /// 日
+  void onChangedDay(String value) {
+    state = AsyncValue.data(state.value!.copyWith(day: int.parse(value)));
+  }
+
+  /// 時間
+  void onChangedHour(String value) {
+    state = AsyncValue.data(state.value!.copyWith(hour: int.parse(value)));
+  }
+
+  /// 分
+  void onChangedMinute(String value) {
+    state = AsyncValue.data(state.value!.copyWith(minute: int.parse(value)));
+  }
+
+  /// タスク
   void onChangedTask(String value) {
     state = AsyncValue.data(state.value!.copyWith(task: value));
   }

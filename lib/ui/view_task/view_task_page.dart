9502e3ea7192b2fc376ba5329a5f6a7e756aad9c
kei1115 <keifukuda1115@gmail.com>
2022-03-08 04:41:28 +0900

    Add TabBar and Rewrite by using stream

--- lib/ui/view_task/view_task_page.dart
+++ lib/ui/view_task/view_task_page.dart
@@ -1,45 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:task_manager/main.dart';
+import 'package:task_manager/data/repository/firestore/firestore_tasks_stream.dart';
 import 'package:task_manager/ui/top/components/task_card.dart';
-import 'package:task_manager/ui/top/top_page.dart';
-import 'package:task_manager/ui/view_task/view_task_view_model.dart';
 
 class ViewTaskPage extends ConsumerWidget {
   const ViewTaskPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final tasks = ref.watch(itemsStreamProvider);
-    final state = ref.watch(viewTaskViewModelProvider);
-    final viewModel = ref.watch(viewTaskViewModelProvider.notifier);
-    return state.when(
-      data: (data) {
-        return Container(
-          padding: EdgeInsets.all(8),
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height - 136,
-          child: tasks.when(
-            data: (items) {
-              List<Widget> tasks = [];
-              items.taskData.forEach((element) {
-                tasks.add(TaskCard(taskData: element));
-              });
-              return Column(
-                children: [
-                  ListView(
-                    children: tasks.reversed.toList(),
-                  ),
-                ],
-              );
-            },
-            error: (error, _) => Text(error.toString()),
-            loading: () => const CircularProgressIndicator(),
+    final tasks = ref.watch(tasksStreamProvider);
+        return Scaffold(
+          appBar: AppBar(
+            toolbarHeight: 0,
+            bottom: TabBar(
+              tabs: [
+                Tab(text: "期限前"),
+                Tab(text: "期限切れ"),
+                Tab(text: "期限なし"),
+              ],
+            ),
+          ),
+          body: Container(
+            padding: EdgeInsets.all(8),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height - 136,
+            child: tasks.when(
+              data: (data) {
+                final taskData = data.taskData;
+                List<Widget> before = [];
+                List<Widget> after = [];
+                List<Widget> no = [];
+
+                taskData.forEach((element) {
+                  var now = DateTime.now();
+                  if (element['noLimit'] == true) {
+                    no.add(TaskCard(taskData: element));
+                  } else if (now.isAfter(element['limit'])) {
+                    after.add(TaskCard(taskData: element));
+                  } else if (now.isBefore(element['limit'])) {
+                    before.add(TaskCard(taskData: element));
+                  }
+                });
+
+                return TabBarView(
+                  children: [
+                    ListView(children: before),
+                    ListView(children: after),
+                    ListView(children: no),
+                  ],
+                );
+              },
+              error: (error, _) => Text(error.toString()),
+              loading: () => const CircularProgressIndicator(),
+            ),
           ),
         );
-      },
-      error: (e, stackTrace) => TopPage(),
-      loading: () => const TopPage(),
-    );
+
   }
 }

30ff0fe9fca8f89ff5d260db86d868af673f4f25
kei1115 <keifukuda1115@gmail.com>
2022-03-07 22:14:01 +0900

    Rewrite TopPage

--- lib/ui/top/top_page.dart
+++ lib/ui/top/top_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:task_manager/ui/components/layout/default.dart';
-import 'package:task_manager/ui/top/top_state.dart';
+import 'package:task_manager/ui/components/appbar.dart';
 import 'package:task_manager/ui/top/top_view_model.dart';
 
 class TopPage extends ConsumerWidget {
@@ -11,10 +10,39 @@ class TopPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(topViewModelProvider);
     final viewModel = ref.watch(topViewModelProvider.notifier);
-    return DefaultLayout<TopState>(
-        state: state,
-        backgroundColor: Colors.black,
-        builder: (data) => Column(),
+    return state.when(
+      data: (data) {
+        return Scaffold(
+          appBar: CustomAppbar(title: '',),
+          body: Column(
+            children: [
+              data.page,
+
+              BottomNavigationBar(
+                onTap: (index) => viewModel.onPageChanged(index),
+                fixedColor: Colors.orange,
+                currentIndex: data.pageIndex,
+                items: [
+                  BottomNavigationBarItem(
+                      label: "1日",
+                      icon: Icon(Icons.ac_unit, color: Colors.orange)),
+                  BottomNavigationBarItem(
+                      label: "3日",
+                      icon: Icon(Icons.ac_unit, color: Colors.orange)),
+                  BottomNavigationBarItem(
+                      label: "7日",
+                      icon: Icon(Icons.ac_unit, color: Colors.orange,)),
+                  BottomNavigationBarItem(
+                      label: "一覧",
+                      icon: Icon(Icons.ac_unit, color: Colors.orange,)),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+      error: (e, stackTrace) => TopPage(),
+      loading: () => const TopPage(),
     );
   }
 }

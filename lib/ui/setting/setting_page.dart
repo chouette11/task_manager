80d31a2f1f2e31984014d5fc59ae9075190b5ab1
kei1115 <keifukuda1115@gmail.com>
2022-05-04 17:33:57 +0900

    add: google signIn

--- lib/ui/setting/setting_page.dart
+++ lib/ui/setting/setting_page.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_signin_button/button_view.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:home_widget/home_widget.dart';
 import 'package:task_manager/ui/components/appbar.dart';
 import 'package:task_manager/ui/components/drawer/drawer.dart';
 import 'package:task_manager/ui/components/layout/default.dart';
@@ -25,11 +28,39 @@ class SettingPage extends ConsumerWidget {
             children: [
               SignInButton(
                 Buttons.GoogleDark,
-                onPressed: () {},
+                onPressed: () async {
+                  final google = await GoogleSignIn(scopes: [
+                    'email',
+                    'https://www.googleapis.com/auth/contacts.readonly',
+                    'https://www.googleapis.com/auth/calendar',
+                  ]).signIn();
+                  final auth = await google!.authentication;
+                  final credential = GoogleAuthProvider.credential(
+                    accessToken: auth.accessToken,
+                    idToken: auth.idToken,
+                  );
+                  print(FirebaseAuth.instance.signInWithCredential(credential));
+                  _updateWidget();
+                }
               ),
             ],
           );
         }
     );
   }
+}
+
+void _updateWidget() {
+  HomeWidget.updateWidget(
+      name: "SmallWidget",
+      androidName: "WidgetProvider",
+      iOSName: "SmallWidget");
+  HomeWidget.updateWidget(
+      name: "MediumWidget",
+      androidName: "WidgetProvider",
+      iOSName: "MediumWidget");
+  HomeWidget.updateWidget(
+      name: "LargeWidget",
+      androidName: "WidgetProvider",
+      iOSName: "LargeWidget");
 }

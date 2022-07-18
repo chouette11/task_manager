6c06f44fedce8882b5174c66ad4a33498e0d20c3
kei1115 <keifukuda1115@gmail.com>
2022-07-18 15:12:45 +0900

    Add onChangeSliderValue and onAddTaskToPieData functions

--- lib/ui/make_schedule/make_schedule_view_model.dart
+++ lib/ui/make_schedule/make_schedule_view_model.dart
@@ -25,4 +25,29 @@ class MakeScheduleViewModel extends StateNotifier<AsyncValue<MakeScheduleState>>
       )
     );
   }
+
+  void onChangeSliderValue(double value) {
+    state = AsyncValue.data(state.value!.copyWith(currentSliderValue: value));
+  }
+
+  void onAddTaskToPieData(String task) {
+    final taskMap = {
+      "domain": task,
+      "measure": state.value!.currentSliderValue
+    };
+    double measureSum = 0;
+    // pieDataの複製
+    var tmpPieData = List.of(state.value!.pieData);
+    // その他の削除
+    tmpPieData.removeLast();
+    // 新規タスクの追加
+    tmpPieData.add(taskMap);
+    // 現在のタスクの合計
+    tmpPieData.forEach((e) {
+      measureSum += e['measure'];
+    });
+    // その他の追加
+    tmpPieData.add({"domain": "その他", "measure": 24 - measureSum});
+    state = AsyncValue.data(state.value!.copyWith(pieData: tmpPieData));
+  }
 }

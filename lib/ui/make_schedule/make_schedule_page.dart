10a04e6859586dd030369f2567ced47477990deb
kei1115 <keifukuda1115@gmail.com>
2022-07-24 02:25:53 +0900

    Fix UI

--- lib/ui/make_schedule/make_schedule_page.dart
+++ lib/ui/make_schedule/make_schedule_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:task_manager/ui/make_schedule/components/pie_chart.dart';
 import 'package:task_manager/ui/make_schedule/make_schedule_view_model.dart';
 import 'package:task_manager/ui/top/top_page.dart';
@@ -14,58 +15,98 @@ class MakeSchedulePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(makeScheduleViewModelProvider);
     final viewModel = ref.watch(makeScheduleViewModelProvider.notifier);
-    final mediaSize = MediaQuery.of(context).size;
     return state.when(
       data: (data) {
-        return Scaffold(
-          body: Column(
-            children: [
-              CustomPieChart(
-                pieData: data.pieData,
-                colors: data.pieColors,
-                legends: data.pieLegends,
-                initialAngle: viewModel.timeToAngle(getUpTime),
-              ),
-              Container(
-                width: mediaSize.width,
-                height: 280,
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  childAspectRatio: 1.5,
-                  children: data.taskData.map((task) =>
-                      GestureDetector(
-                        onTap: () => viewModel.onAddTaskToPieData(task.title),
-                        child: TaskCard(task: task),
-                      ),
-                  ).toList(),
-                ),
-              ),
-              Slider(
-                value: data.currentSliderValue,
-                max: 4,
-                divisions: 8,
-                label: "${data.currentSliderValue.toString()}" + "時間",
-                onChanged: viewModel.onChangeSliderValue,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+        return SafeArea(
+          child: Scaffold(
+            body: Padding(
+              padding: const EdgeInsets.only(top: 16, right: 8, left: 8, bottom: 16),
+              child: Column(
                 children: [
-                  Text("休憩"),
-                  ElevatedButton(
-                    onPressed: () => viewModel.onAddTaskToPieData('休憩', hours: 0.25),
-                    child: Text("15分"),
+                  CustomPieChart(
+                    pieData: data.pieData,
+                    colors: data.pieColors,
+                    legends: data.pieLegends,
+                    initialAngle: viewModel.timeToAngle(getUpTime),
                   ),
-                  ElevatedButton(
-                    onPressed: () => viewModel.onAddTaskToPieData('休憩', hours: 0.5),
-                    child: Text("30分"),
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      childAspectRatio: 1.5,
+                      children: data.taskData.map((task) =>
+                          GestureDetector(
+                            onTap: () => viewModel.onAddTaskToPieData(task.title),
+                            child: TaskCard(task: task),
+                          ),
+                      ).toList(),
+                    ),
                   ),
-                  ElevatedButton(
-                    onPressed: () => viewModel.onAddTaskToPieData('休憩', hours: 1),
-                    child: Text("1時間"),
+                  SizedBox(height: 8),
+                  SizedBox(
+                    height: 32,
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16.0),
+                          child: Text("時間"),
+                        ),
+                        Expanded(
+                          child: Slider(
+                            value: data.currentSliderValue,
+                            max: 4,
+                            divisions: 8,
+                            label: "${data.currentSliderValue.toString()}" + "時間",
+                            onChanged: viewModel.onChangeSliderValue,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
+                  SizedBox(height: 8),
+                  SizedBox(
+                    height: 32,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 16, right: 16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("休憩"),
+                          ElevatedButton(
+                            onPressed: () => viewModel.onAddTaskToPieData('休憩', hours: 0.25),
+                            child: Text("15分"),
+                          ),
+                          ElevatedButton(
+                            onPressed: () => viewModel.onAddTaskToPieData('休憩', hours: 0.5),
+                            child: Text("30分"),
+                          ),
+                          ElevatedButton(
+                            onPressed: () => viewModel.onAddTaskToPieData('休憩', hours: 1),
+                            child: Text("1時間"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  SizedBox(
+                    height: 48,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () => context.pop,
+                          child: Text("戻る"),
+                        ),
+                        ElevatedButton(
+                          onPressed: () => context.go('/'),
+                          child: Text("就寝"),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
-              )
-            ],
+              ),
+            ),
           ),
         );
       },
